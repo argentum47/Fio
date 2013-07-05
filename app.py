69caf753dc8622cb2fc0695bc2b24b733fe48899
Elise Hein <elisehein@gmail.com>
2013-07-05 10:40:20 +0100

    Don't mix up jinja and angular

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 app = Flask(__name__)
 
 @app.route('/')
 def data_table():
-   return render_template('index.html')
+   return make_response(open('templates/index.html').read())
 
 if __name__ == '__main__':
     app.run()
