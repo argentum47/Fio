a0cf7791d97f7cebffb01fd522d33d50dd5a9576
Elise Hein <elisehein@gmail.com>
2013-07-10 15:54:15 +0100

    Add press-enter directive so that we don't need a submit button for forms

--- static/js/app.js
+++ static/js/app.js
@@ -206,3 +206,16 @@ app.factory('DataServices', function($filter) {
 
   return s;
 });
+
+angular.module('fio').directive('pressEnter', function() {
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      if(event.which === 13) {
+        scope.$apply(function(){
+          scope.$eval(attrs.pressEnter);
+        });
+        event.preventDefault();
+      }
+    });
+  };
+});
