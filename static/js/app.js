1065750010037091a95ded1ed4ca93cb4c4fc923
Elise Hein <elisehein@gmail.com>
2013-07-11 13:47:20 +0100

    Basic routing

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,7 @@
-var app = angular.module('fio', ['fio.services', 'fio.controllers', 'fio.directives']);
+var app = angular.module('fio', ['fio.services', 'fio.controllers', 'fio.directives'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/io', {templateUrl: 'static/partials/io.html', controller: 'IOCtrl'});
+    $routeProvider.when('/recurring', {templateUrl: 'static/partials/recurring.html'});
+    $routeProvider.when('/totals', {templateUrl: 'static/partials/totals.html'});
+    $routeProvider.otherwise({redirectTo: '/'});
+  }]);
