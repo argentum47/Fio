3507224307351995de4fb5d9a843449dc5ebb762
Elise Hein <elisehein@gmail.com>
2013-07-11 19:23:48 +0100

    Add filters stub

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('fio', ['fio.services', 'fio.controllers', 'fio.directives'])
+var app = angular.module('fio', ['fio.services', 'fio.controllers', 'fio.directives', 'fio.filters'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/io', {templateUrl: 'static/partials/io.html', controller: 'IOCtrl'});
   $routeProvider.when('/recurring', {templateUrl: 'static/partials/recurring.html'});
