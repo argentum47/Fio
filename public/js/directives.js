4ee0ce7707097064c411bb8db51261c06d2d88fa
Elise Hein <elisehein@gmail.com>
2013-07-29 18:12:53 +0100

    Closer to solving edit state issue with working remove button

--- public/js/directives.js
+++ public/js/directives.js
@@ -3,43 +3,56 @@ angular.module('fio.directives', [])
 // Directive that executes expression when the FORM it is applied to either
 // detects an ENTER keypress
 // or detects that all of it's inputs have lost focus
-.directive('finaliseEditing', function($timeout) {
+.directive('interestLost', function($timeout) {
   return function(scope, element, attrs) {
 
-    // Unfocus focused input on ENTER keypress
-    // (assuming only one input can have focus at any one time)
-    element.find('input').bind("keydown keypress", function(event) {
-      if(event.which === 13) {
-        element.find('input:focus').blur();
+    // Execute when interest has been lost
+    var interest_lost = function() {
+      // Only allow execution if the form is valid
+      if (!$(element).find('form').hasClass('ng-invalid')) {     
+        scope.$apply(function() {
+          scope.$eval(attrs.interestLost);
+        });
+      }     
+    }
+
+    // All the inputs we need to watch
+    var inputs = element.find('input');
+
+    // Lose interest on ENTER or ESC keypress,
+    // or TAB keypress if it is the last input in the element
+    inputs.bind("keydown", function(event) {
+      var key = event.keyCode || event.which;
+      var is_last_input = $(this).is(inputs[inputs.length - 1]);
+      if(key === 13 || key === 27 || 
+         (key === 9 && is_last_input)) {
+        interest_lost();
       }
     });
 
-    // Listen to any contained inputs losing focus
-    element.find('input').bind('blur', function(event) {
-      
+    // Lose interest when no inputs have focus
+    inputs.bind('blur', function(event) {
+
       // Give it a moment to allow any other inputs to get focus
       $timeout(function() {
+        if (!inputs.is(':focus')) {
 
-        // Only okay to execute expression when sure that
-        // no other inputs in the same form now have focus
-        // and that all required stuff has been filled out
-        if (!$(element).find('input').is(':focus')
-            && !$(element).hasClass('ng-invalid')) {
+          setTimeout(function() {
+            // You can't lose interest
+            // if the element isn't there
+            if ($('html').find(element).length) {
+              interest_lost();
+            }
+          }, 500);
 
-          scope.$apply(function() {
-            scope.$eval(attrs.finaliseEditing);
-          });
-
-        } /* if no other has blur and is valid */
-
+        } /* if no other has focus */
       }); /* timeout */
     }); /* input bind blur */
-  }; /* return function */
-}) /* stopEditing */
 
-// Focus the first input contained in the directive applied to
-// unless other inputs of the form have focus
-// (this would mean we are still editing the entry)
+  }; /* return function */
+}) /* interestLost */
+
+// Execute the expression when enter is pressed
 .directive('pressEnter', function($timeout) {
   return function(scope, element, attrs) {
     element.bind("keydown keypress", function(event) {
@@ -53,7 +66,9 @@ angular.module('fio.directives', [])
   };
 })
 
-// Execute the expression when enter is pressed
+// Focus the first input contained in the directive applied to
+// unless other inputs of the form have focus
+// (this would mean we are still editing the entry)
 .directive('focusOnClick', function($timeout) {
   return {
     link: function(scope, element, attrs) {
